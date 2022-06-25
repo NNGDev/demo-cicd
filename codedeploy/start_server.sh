a94a2b5f147019c1beeea2f3711222885775ee1e
Hai <n.ngochai@yahoo.de>
2022-06-25 22:56:24 +0200

    artifacts

--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -2,5 +2,5 @@
 # source /home/ec2-user/web/.env
 
 # $(aws ecr get-login --no-include-email --region $REGION)
-
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 141730029070.dkr.ecr.us-east-1.amazonaws.com
 cd /home/ec2-user/web/ && /usr/local/bin/docker-compose -f docker-compose.yml up -d
